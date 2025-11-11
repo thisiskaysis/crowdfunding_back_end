e20059e5748c915beb2da6c9876aaf4c11828061
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-11 19:14:59 +1000

    created first half of views content (one class in views.py)

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Fundraiser
+from .serializers import FundraiserSerializer
+
+class FundraiserList(APIView):
+    def get(self, request):
+        fundraisers = Fundraiser.objects.all()
+        serializer = FundraiserSerializer(fundraisers, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = FundraiserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
