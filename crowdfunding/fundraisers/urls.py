efc656f122a17c6db8f83ddf38cd6f14f8d14bf9
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-11 20:23:14 +1000

    completed second half of views content

--- crowdfunding/fundraisers/urls.py
+++ crowdfunding/fundraisers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fundraisers/', views.FundraiserList.as_view())
+    path('fundraisers/', views.FundraiserList.as_view()),
+    path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
 ]
--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -26,3 +26,16 @@ class FundraiserList(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+class FundraiserDetail(APIView):
+    def get_object(self, pk):
+        try:
+            fundraiser = Fundraiser.objects.get(pk=pk)
+            return fundraiser
+        except Fundraiser.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        fundraiser = self.get_object(pk)
+        serializer = FundraiserSerializer(fundraiser)
+        return Response(serializer.data)
