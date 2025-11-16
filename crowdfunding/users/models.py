833a8183854f766e7e425055946b2f6a9605f4cf
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-17 00:11:02 +1000

    written users models only (with admin file updated)

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,25 +1,22 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
-    
-class Parent(AbstractUser):
+from django.conf import settings
+
+class CustomUser(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    date_of_birth = models.DateField(null=True, blank=True)
-    address = models.CharField(max_length=255, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
-    def __str__(self):
-        return self.username
-    
-class Child(AbstractUser):
-    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='children')
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     date_of_birth = models.DateField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
     def __str__(self):
         return self.username
+
+class Child(models.Model):
+    parent = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='children')
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    def __str__(self):
+        return f"{self.name} ({self.parent.username})"
+
--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.CustomUserList.as_view()),
-    path('users/<int:pk>/', views.CustomUserDetail.as_view())
+    
 ]
