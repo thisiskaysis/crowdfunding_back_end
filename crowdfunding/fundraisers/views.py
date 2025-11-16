82194efcc39d8c4a60eefd6e8e871ff1c0047527
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-17 01:18:43 +1000

    not sure lol

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -7,7 +7,6 @@ from .models import Campaign, Donation
 from .serializers import CampaignSerializer, DonationSerializer, CampaignDetailSerializer
 
 # ------ Campaign Views ------
-
 class CampaignList(APIView):
     # List all campaigns or create a new one
     def get(self, request, format=None):
@@ -47,18 +46,3 @@ class CampaignDetail(APIView):
         campaign = self.get_object(pk)
         campaign.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-# ------ Donation Views ------
-class DonationList(APIView):
-    # List all donations or create a new one
-    def get(self, request, format=None):
-        donations = Donation.objects.all()
-        serializer = DonationSerializer(donations, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = DonationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
