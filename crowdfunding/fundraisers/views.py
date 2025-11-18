1486b64bcb7a8d368be31fbc0c03b5d432bde155
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-18 11:05:51 +1000

    this is a message about my changes. I have made changes to my views.py file, adding donations

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -46,3 +46,18 @@ class CampaignDetail(APIView):
         campaign = self.get_object(pk)
         campaign.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# ------ Donation Views ------
+class DonationList(APIView):
+    # List all donations or create a new one
+    def get(self, request, format=None):
+        donations = Donation.objects.all()
+        serializer = DonationSerializer(donations, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = DonationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
