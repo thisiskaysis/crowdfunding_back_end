9355bc0358e14c5a16ce76d3d865185cf1e659ed
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-22 10:00:12 +1000

    deleted databse, deleted migration files, and re-migrated

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -4,12 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
-    is_verified = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.username
     
@@ -17,5 +12,6 @@ class Child(models.Model):
     parent = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='children')
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
+    profile_picture = models.ImageField(upload_to='child_profile_pics/', null=True, blank=True)
     def __str__(self):
         return self.name
